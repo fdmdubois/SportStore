48cb5d74ed869f4803be3f39cf5a17853a42bcac
Frans Dubois <fdmdubois@gmail.com>
2015-06-01 21:25:20 +0200

    Eerste comm

---
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+
+        private EfDbContext context = new EfDbContext();
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
